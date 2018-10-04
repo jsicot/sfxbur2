04936cf888bdb110fc6e73514a129e0c7aebe180
sicot_j <julien.sicot@univ-rennes2.fr>
2018-10-04 17:12:01 +0200

    modified CAIRN parser to use only openurl service

--- lib/Parsers/TargetParser/CAIRN/CAIRN.pm
+++ lib/Parsers/TargetParser/CAIRN/CAIRN.pm
@@ -11,50 +11,64 @@ sub getFullTxt
 	my $ctx_obj     = $this->{ctx_obj};
 	my $svc         = $this->{svc};
 	my $jkey        = $svc->parse_param('jkey');
-	my $host1       = $svc->parse_param('url');
-	my $host2       = "http://www.cairn.info/revue";
-        my $exception1       = $svc->parse_param('exception1');
+	my $host        = $svc->parse_param('url');
+        my $exception1  = $svc->parse_param('exception1');
 
-	return $this->getCAIRNURL($ctx_obj, $jkey,$host1,$host2,$exception1);
+	return $this->getCAIRNURL($ctx_obj, $jkey,$host,$exception1);
 }
 
 
 sub getCAIRNURL
 {
-	my ($this, $ctx_obj, $jkey,$host1,$host2,$exception1) = @_;
+	my ($this, $ctx_obj, $jkey,$host,$exception1) = @_;
 	
 	my %query = (); 
 	my $publication_year  = $ctx_obj->get('rft.year')  || '';
+	my $volume            = $ctx_obj->get('rft.volume') || '';
 	my $issue             = $ctx_obj->get('rft.issue') || ''; 
 	my $page              = $ctx_obj->get('rft.spage') || '';
+	my $issn	      = $ctx_obj->get('rft.eissn') || '';
+	my $isbn	      = $ctx_obj->get('rft.isbn_13') || '';
 	my $doi               = $ctx_obj->get('rft.doi')   || '';
 	my $doiurl            = $ctx_obj->get('sfx.doi_url');
-	my $atitle = $ctx_obj->get('rft.atitle') || '';
+	my $atitle	      = $ctx_obj->get('rft.atitle') || '';
 	my $uri;
 	
-	$uri = URI->new($host1);
+	$uri = URI->new($host);
 
 	if ($atitle && $exception1 && $exception1 eq 'atitle'){
                 $query{'send_search_field'}   = "Chercher";
                 $query{'searchTerm'}   = "\"$atitle\"";
                 $query{'searchIn'}   = "all";
-                $uri = URI->new($host1."resultats_recherche.php");
+                $uri = URI->new($host."resultats_recherche.php");
         }
 	elsif($doi && $doiurl){
-		if ($this->is_doi_allowed('CAIRN::CAIRN',$doi)) {
-			$uri = URI->new("http://www.cairn.info/OpenUrl/sap.php?doi=$doi");
-		} elsif (length($jkey) && length($publication_year) && length($issue) && length($page)) {
-			$uri = URI->new("$host2-$jkey-$publication_year-$issue-page-$page.htm");
+		if ($this->is_doi_allowed('CAIRN::CAIRN2',$doi)) {
+			$uri = URI->new("$host/OpenUrl/sap.php?doi=$doi");
+		} elsif (length($publication_year) && length($volume) && length($issue) && length($page)) {
+           		 $uri = URI->new("$host/	OpenUrl/sap.php?issn=$issn&date=$publication_year&volume=$volume&issue=$issue&spage=$page");
 		}
 	}
-	elsif (length($jkey) && length($publication_year) && length($issue) && length($page)) {
-            $uri = URI->new("$host2-$jkey-$publication_year-$issue-page-$page.htm");
+	elsif(length($isbn)){
+            $uri = URI->new("$host/OpenUrl/sap.php?isbn=$isbn");
+	}
+	elsif (length($issn) && length($publication_year) && length($volume) && length($issue) && length($page)) {
+            $uri = URI->new("$host/OpenUrl/sap.php?issn=$issn&date=$publication_year&volume=$volume&issue=$issue&spage=$page");
         }
-	elsif (length($jkey) && length($publication_year) && length($issue)) {
-            $uri = URI->new("$host2-$jkey-$publication_year-$issue.htm");
+	 elsif (length($jkey) && length($publication_year) && length($issue) && length($page)) {
+             $uri = URI->new("$host-$jkey-$publication_year-$issue-page-$page.htm");
+	 }
+	elsif (length($issn) && length($publication_year) && length($volume) && length($issue)) {
+            $uri = URI->new("$host/OpenUrl/sap.php?issn=$issn&date=$publication_year&volume=$volume&issue=$issue");
         }
-	elsif (length($jkey)) {
-            $uri = URI->new("$host2-$jkey.htm");
+	elsif (length($issn) && length($publication_year) && length($volume)) {
+            $uri = URI->new("$host/OpenUrl/sap.php?issn=$issn&date=$publication_year&volume=$volume");
+        }
+	elsif (length($issn) && length($publication_year)) {
+            $uri = URI->new("$host/OpenUrl/sap.php?issn=$issn&date=$publication_year");
+        }
+	elsif (length($issn)) {
+            $uri = URI->new("$host/OpenUrl/sap.php?issn=$issn");
         }
 	
 	$uri->query_form(%query);
